ee55ab66d7154705333e441d7b74f93e5d2b4fdb
Michael Robbins <michael+github@dalmura.com.au>
2025-11-04 20:59:53 +1100

    Fixing up scripts to create a bunch more movies to test scrolling issues

--- scripts/create_agents.sh
+++ scripts/create_agents.sh
@@ -27,8 +27,15 @@ ${DATA_SCRIPT} "${TARGET_DIR}/agent3"
 # Delete some specific things for each agent
 
 ## agent1 is missing
-# Movie B
 rm -rf "${TARGET_DIR}/agent1/movies/Movie B (2024)"
+rm -rf "${TARGET_DIR}/agent1/movies/Movie D (2020)"
+rm -rf "${TARGET_DIR}/agent1/movies/Movie F (2022)"
+rm -rf "${TARGET_DIR}/agent1/movies/Movie H (2018)"
+rm -rf "${TARGET_DIR}/agent1/movies/Movie K (2015)"
+rm -rf "${TARGET_DIR}/agent1/movies/Movie M (2013)"
+rm -rf "${TARGET_DIR}/agent1/movies/Movie P (1992)"
+rm -rf "${TARGET_DIR}/agent1/movies/Movie S (2007)"
+rm -rf "${TARGET_DIR}/agent1/movies/Movie V (2014)"
 
 # Show A (entirely)
 rm -rf "${TARGET_DIR}/agent1/tv/Show A (1989)"
@@ -41,8 +48,14 @@ rm -rf "${TARGET_DIR}/agent1/tv/Show C (1999)/Season 1/S01E03 - Ep 3 (1999).mkv"
 
 
 ## agent2 is missing
-# Movie C
 rm -rf "${TARGET_DIR}/agent2/movies/Movie C (2025)"
+rm -rf "${TARGET_DIR}/agent2/movies/Movie E (2021)"
+rm -rf "${TARGET_DIR}/agent2/movies/Movie G (2019)"
+rm -rf "${TARGET_DIR}/agent2/movies/Movie J (2020)"
+rm -rf "${TARGET_DIR}/agent2/movies/Movie L (2014)"
+rm -rf "${TARGET_DIR}/agent2/movies/Movie O (2011)"
+rm -rf "${TARGET_DIR}/agent2/movies/Movie R (2001)"
+rm -rf "${TARGET_DIR}/agent2/movies/Movie U (2012)"
 
 # Show A / Season 1 (entirely)
 rm -rf "${TARGET_DIR}/agent2/tv/Show A (1989)/Season 1"
@@ -51,7 +64,17 @@ rm -rf "${TARGET_DIR}/agent2/tv/Show A (1989)/Season 1"
 rm -rf "${TARGET_DIR}/agent2/tv/Show B (1994)/Season 3/S03E01 - Ep 1 (1994).mkv"
 
 
-## agent3 has everything
+## agent3 is missing
+rm -rf "${TARGET_DIR}/agent3/movies/Movie A (2023)"
+rm -rf "${TARGET_DIR}/agent3/movies/Movie D (2020)"
+rm -rf "${TARGET_DIR}/agent3/movies/Movie E (2021)"
+rm -rf "${TARGET_DIR}/agent3/movies/Movie F (2022)"
+rm -rf "${TARGET_DIR}/agent3/movies/Movie I (2017)"
+rm -rf "${TARGET_DIR}/agent3/movies/Movie K (2015)"
+rm -rf "${TARGET_DIR}/agent3/movies/Movie N (2019)"
+rm -rf "${TARGET_DIR}/agent3/movies/Movie Q (1983)"
+rm -rf "${TARGET_DIR}/agent3/movies/Movie T (2006)"
+rm -rf "${TARGET_DIR}/agent3/movies/Movie W (2003)"
 
 
 find "${TARGET_DIR}"
--- scripts/create_fake_data.sh
+++ scripts/create_fake_data.sh
@@ -17,6 +17,66 @@ head -c 20M </dev/urandom >"${TARGET_DIR}/movies/Movie B (2024)/Movie B (2024).m
 mkdir -p "${TARGET_DIR}/movies/Movie C (2025)"
 head -c 30M </dev/urandom >"${TARGET_DIR}/movies/Movie C (2025)/Movie C (2025).mkv"
 
+mkdir -p "${TARGET_DIR}/movies/Movie D (2020)"
+head -c 15M </dev/urandom >"${TARGET_DIR}/movies/Movie D (2020)/Movie D (2020).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie E (2021)"
+head -c 18M </dev/urandom >"${TARGET_DIR}/movies/Movie E (2021)/Movie E (2021).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie F (2022)"
+head -c 22M </dev/urandom >"${TARGET_DIR}/movies/Movie F (2022)/Movie F (2022).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie G (2019)"
+head -c 25M </dev/urandom >"${TARGET_DIR}/movies/Movie G (2019)/Movie G (2019).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie H (2018)"
+head -c 12M </dev/urandom >"${TARGET_DIR}/movies/Movie H (2018)/Movie H (2018).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie I (2017)"
+head -c 16M </dev/urandom >"${TARGET_DIR}/movies/Movie I (2017)/Movie I (2017).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie J (2020)"
+head -c 20M </dev/urandom >"${TARGET_DIR}/movies/Movie J (2020)/Movie J (2020).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie K (2015)"
+head -c 14M </dev/urandom >"${TARGET_DIR}/movies/Movie K (2015)/Movie K (2015).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie L (2014)"
+head -c 19M </dev/urandom >"${TARGET_DIR}/movies/Movie L (2014)/Movie L (2014).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie M (2013)"
+head -c 23M </dev/urandom >"${TARGET_DIR}/movies/Movie M (2013)/Movie M (2013).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie N (2019)"
+head -c 17M </dev/urandom >"${TARGET_DIR}/movies/Movie N (2019)/Movie N (2019).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie O (2011)"
+head -c 21M </dev/urandom >"${TARGET_DIR}/movies/Movie O (2011)/Movie O (2011).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie P (1992)"
+head -c 13M </dev/urandom >"${TARGET_DIR}/movies/Movie P (1992)/Movie P (1992).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie Q (1983)"
+head -c 24M </dev/urandom >"${TARGET_DIR}/movies/Movie Q (1983)/Movie Q (1983).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie R (2001)"
+head -c 11M </dev/urandom >"${TARGET_DIR}/movies/Movie R (2001)/Movie R (2001).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie S (2007)"
+head -c 26M </dev/urandom >"${TARGET_DIR}/movies/Movie S (2007)/Movie S (2007).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie T (2006)"
+head -c 28M </dev/urandom >"${TARGET_DIR}/movies/Movie T (2006)/Movie T (2006).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie U (2012)"
+head -c 15M </dev/urandom >"${TARGET_DIR}/movies/Movie U (2012)/Movie U (2012).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie V (2014)"
+head -c 27M </dev/urandom >"${TARGET_DIR}/movies/Movie V (2014)/Movie V (2014).mkv"
+
+mkdir -p "${TARGET_DIR}/movies/Movie W (2003)"
+head -c 29M </dev/urandom >"${TARGET_DIR}/movies/Movie W (2003)/Movie W (2003).mkv"
+
 touch "${TARGET_DIR}/movies/.stfolder"
 mkdir "${TARGET_DIR}/movies/.stversions"
 
